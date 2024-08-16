5992b3c1f51e61219a7c45a82513a8ff08a6873f
Sirwan <sir1afifi@gmail.com>
2024-08-16 13:00:05 +0100

    update clean up

--- cleanup.ts
+++ cleanup.ts
@@ -1,38 +1,48 @@
-import * as fs from "fs";
-import * as path from "path";
+import { readdir, unlink } from "fs/promises";
+import { join } from "path";
 
-const getDateOneWeekAgo = () => {
+const getLast7DaysOfMonth = () => {
   const date = new Date();
-  date.setDate(date.getDate() - 7);
-  const formattedDate = new Intl.DateTimeFormat("en-GB").format(date);
-  return formattedDate.replace(/\//g, "-");
+  const lastDayOfMonth = new Date(date.getFullYear(), date.getMonth() + 1, 0);
+  const firstDayOfLast7Days = new Date(lastDayOfMonth);
+  firstDayOfLast7Days.setDate(lastDayOfMonth.getDate() - 6);
+
+  const dates = [];
+  for (
+    let d = firstDayOfLast7Days;
+    d <= lastDayOfMonth;
+    d.setDate(d.getDate() + 1)
+  ) {
+    dates.push(
+      new Intl.DateTimeFormat("en-GB").format(new Date(d)).replace(/\//g, "-")
+    );
+  }
+  return dates;
 };
 
-const deleteOldFiles = () => {
+const deleteOldFiles = async () => {
   const outputDir = "./output";
-  const oneWeekAgo = getDateOneWeekAgo();
+  const last7Days = getLast7DaysOfMonth();
 
-  fs.readdir(outputDir, (err, files) => {
-    if (err) {
-      console.error(`Error reading directory: ${err.message}`);
-      return;
-    }
+  try {
+    const files = await readdir(outputDir);
 
-    files.forEach((file) => {
-      const filePath = path.join(outputDir, file);
+    for (const file of files) {
+      const filePath = join(outputDir, file);
       const fileDate = file.match(/\d{2}-\d{2}-\d{4}/)?.[0];
 
-      if (fileDate && fileDate < oneWeekAgo) {
-        fs.unlink(filePath, (err) => {
-          if (err) {
-            console.error(`Error deleting file ${filePath}: ${err.message}`);
-          } else {
-            console.log(`Deleted file: ${filePath}`);
-          }
-        });
+      if (fileDate && !last7Days.includes(fileDate)) {
+        try {
+          await unlink(filePath);
+          console.log(`Deleted file: ${filePath}`);
+        } catch (err) {
+          console.error(`Error deleting file ${filePath}: ${err.message}`);
+        }
       }
-    });
-  });
+    }
+  } catch (err) {
+    console.error(`Error reading directory: ${err.message}`);
+  }
 };
 
 deleteOldFiles();
