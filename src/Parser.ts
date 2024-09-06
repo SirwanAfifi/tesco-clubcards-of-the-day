4e5e8522e013a57e1ced62a56049e82a228a5441
Sirwan <sir1afifi@gmail.com>
2024-09-06 23:28:42 +0100

    change the fetch to puppeteer

--- src/Parser.ts
+++ src/Parser.ts
@@ -1,5 +1,6 @@
 import * as cheerio from "cheerio";
 import { Product } from "./Product";
+import puppeteer from "puppeteer";
 
 const URL = "https://www.tesco.com/groceries/en-GB/buylists/clubcard-prices";
 
@@ -58,9 +59,44 @@ export class Parser {
   }
 
   private async loadContent(url: string) {
-    const response = await fetch(url);
-    const html = await response.text();
-    return cheerio.load(html);
+    const browser = await puppeteer.launch({
+      headless: true,
+      args: [
+        "--disable-gpu",
+        "--disable-dev-shm-usage",
+        "--disable-setuid-sandbox",
+        "--no-first-run",
+        "--no-sandbox",
+        "--no-zygote",
+        "--deterministic-fetch",
+        "--disable-features=IsolateOrigins",
+        "--disable-site-isolation-trials",
+        "--disable-http2", // Disable HTTP/2
+      ],
+    });
+
+    const page = await browser.newPage();
+
+    await page.setUserAgent(
+      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+    );
+
+    await page.setExtraHTTPHeaders({
+      "Accept-Language": "en-US,en;q=0.9",
+    });
+
+    try {
+      await page.goto(url, {
+        waitUntil: "networkidle0",
+      });
+      const html = await page.content();
+      return cheerio.load(html);
+    } catch (error) {
+      console.error("Error loading page:", error);
+      throw error;
+    } finally {
+      await browser.close();
+    }
   }
 
   private async getLastPageNumber(category: string): Promise<number> {
